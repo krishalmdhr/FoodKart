fe335baa2140b1be6053660b7b6b3888f4034bfc
krishalmdhr <krishalmdhr@gmail.com>
2020-09-27 19:19:13 +0545

    slight front-end changers

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -9,7 +9,8 @@
         <thead>
             <tr>
                 <th>Name</th>
-                <th>Price</th>
+                <th>Rate</th>
+                <th>Total Price</th>
                 {{-- <th>Vendor</th> --}}
                 <th>Quantity</th>
                 <th>Action</th>
@@ -24,8 +25,9 @@
             </tr> --}}
             <tr>
                 <td scope="row">{{$item->name}}</td>
+                <td scope="row">${{$item->price}}</td>
                 <td>
-                    {{Cart::session(auth()->id())->get($item->id)->getPriceSum()}}
+                    ${{Cart::session(auth()->id())->get($item->id)->getPriceSum()}}
                 </td>
                 {{-- <td>
                     {{$item->vendor}}
@@ -46,7 +48,7 @@
     </table>
 
     <h3>
-        Total Price: Rs. {{\Cart::session(auth()->id())->getTotal()}}
+        Total Price: $ {{\Cart::session(auth()->id())->getTotal()}}
     </h3>
 
 <a name="" id="" class="btn btn-primary" href="{{route('cart.checkout')}}" role="button">Checkout</a>
